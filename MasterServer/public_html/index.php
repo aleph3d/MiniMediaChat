73ea12895df0e937bea0eac519d262b986c4dfd8
Hannah Dunitz <hannahyhvh@gmail.com>
2014-03-05 02:20:26 -0600

    corrected file meta header info and added some scripts.

--- MasterServer/public_html/index.php
+++ MasterServer/public_html/index.php
@@ -1,4 +1,7 @@
-<?php
+<? 
+// FILE: index (part of MiniMediaChat, http://aleph3d.com:8082/repos/MiniMediaChat)
+// TYPE: user callable HTML5-Based File (PHP5)
+// LICENSE: MIT (Copyright 2014 Hannah Dunitz)
 function proCheck() { return TRUE; }
 include("path.php");
 include(pathRel."config/mainConfig.php");
